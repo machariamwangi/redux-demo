905cb8b2bf9ac5d15d12b2f7a3b7a8034c08dc92
machariamwangi <elijah.mwangi@binary.co.ke>
2020-07-22 20:14:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const redux = require('redux');
+const reduxLogger = require('redux-logger');
+
+
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger()
+
+
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICE_CREAM = 'BUY_ICE_CREAM'
+// action
+function buyCake() {
+    return{
+     type: BUY_CAKE,
+     info: 'First redux action'
+}
+}
+function buyIceCream() {
+    return{
+       type: BUY_ICE_CREAM,
+       info: 'Second redux action'
+    }
+    
+}
+
+// (previousState, action) => newState
+
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+
+const initialCakeState ={
+    numOfCakes: 10
+}
+const initialIceCreamState ={
+    numOfIceCreams: 20
+}
+//reducer
+// const reducer = (state = initialState, action) =>{
+//     switch (action.type) {
+//         case BUY_CAKE: return{
+//             ...state,
+//             numOfCakes: state.numOfCakes -1
+//         }
+//           case BUY_ICE_CREAM: return{
+//             ...state,
+//             numOfIceCreams: state.numOfIceCreams -1
+//         }    
+//         default:
+//         return state;
+//     }
+// }
+
+const  cakeReducer = (state = initialCakeState, action) =>{
+    switch (action.type) {
+        case BUY_CAKE: return{
+            ...state,
+            numOfCakes: state.numOfCakes -1
+        }   
+        default:
+        return state;
+    }
+}
+const iceCreamReducer = (state = initialIceCreamState, action) =>{
+    switch (action.type) {
+        
+          case BUY_ICE_CREAM: return{
+            ...state,
+            numOfIceCreams: state.numOfIceCreams -1
+        }    
+        default:
+        return state;
+    }
+}
+ //store
+ const rootReducer = combineReducers({
+     cake: cakeReducer,
+     iceCream: iceCreamReducer,
+ })
+const store = createStore(rootReducer, applyMiddleware(logger) )
+
+
+console.log('initial tate', store.getState())
+
+const unsubscribe = store.subscribe( ()=>{})
+
+store.dispatch( buyCake())
+store.dispatch( buyCake())
+store.dispatch( buyCake())
+store.dispatch( buyIceCream())
+store.dispatch( buyIceCream())
+unsubscribe()
